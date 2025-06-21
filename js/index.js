1eab5637263476a60174906ff1f991cfde56e547
SKBWEBDEV <najmujshakib025@gmail.com>
2025-06-21 22:22:33 +0600

    first project

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("ok").innerHTML ="do you like me"
+
+document.getElementsByClassName('ONE')[4].innerHTML ='can you tell me'
+
+// alert('আমরা অনুমতি ব্যাতিত ওয়েব সাইট এ প্রবেশ নিষেধ')
+
+console.log('how can i help you')
